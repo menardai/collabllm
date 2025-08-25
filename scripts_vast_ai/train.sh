33039db1cdff8d66260110298f981ded39fd01dc
Stephane Menard <stephanemenard211@gmail.com>
2025-08-25 09:57:44 -0400

    update train script

--- scripts_vast_ai/train.sh
+++ scripts_vast_ai/train.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 cd /data/collabllm
+export OPENAI_API_KEY=fake_key
+export WANDB_MODE=disabled
 CUDA_VISIBLE_DEVICES=0 WANDB__SERVICE_WAIT=300 torchrun --master_port=56500 --nnodes=1 --nproc_per_node=1 -m scripts.train.offline_dpo \
     --dataset_repo collabllm/collabllm-multiturn-medium \
     --model_name meta-llama/Llama-3.2-3B-Instruct \
-    --output_dir outputs/offline_dpo_from_base/collabllm-multiturn-medium \
+    --output_dir outputs/offline_dpo_from_base/collabllm-multiturn-medium \ 
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 8 \
